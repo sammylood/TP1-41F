8220fc2508d73ea642e1b41a022d3a9be67c5556
Sammylood <sammy.romeo@hotmail.com>
2025-02-26 12:05:55 -0500

    commit final

--- data/dataTest.js
+++ data/dataTest.js
@@ -1,11 +1,11 @@
 const evenementsTest = [
         {
-          "id":11129128,
-          "type":"Concert",
-          "genre": "Rock",
+          "id": "11129128",
+          "type": "Concert",
+          "titre": "Wild Flag at The Fillmore",
+          "genre": "rock",
           "poster": "poster-Wild-flag.jpg",
-          "uri":"11129128-wild-flag-at-fillmore",
-          "titre":"Wild Flag at The Fillmore (April 18, 2025)",
+          "uri": "11129128-wild-flag-at-fillmore",
           "artiste": [
             {  
                 "artiste_id": 29835,
@@ -13,10 +13,206 @@ const evenementsTest = [
                 "artiste_nom":"Wild Flag"
               }
             ],
+            "date": "2025-04-01T20:00:00-0800",
+            "ville": "Longueuil",
+            "lieu": "Théâtre de la Ville",
+            "prix": "0",
+             "participants": [
+            {  
+                "idInvite": 51409,
+                "interessé":"2",
+              },
+               {  
+                "idInvite": 51402,
+                "interessé":"3",
+              },
+               {  
+                "idInvite": 51406,
+                "interessé":"2",
+              },
+               {  
+                "idInvite": 51407,
+                "interessé":"2",
+              }
+            ]
+          },
+          {
+            "id": "11129129",
+            "type": "Concert",
+            "titre": "Claude Hurtubise à la Maison de la Culture Ahuntsic-Cartierville",
+            "genre": "folk",
+            "poster": "poster-claude-hurtubise.jpg",
+            "uri": "11129129-claude-hurtubise",
+            "artiste": [
+            {  
+                "artiste_id": 28665,
+                "artiste_uri":"28665-claude-hurtubise",
+                "artiste_nom":"Claude Hurtubise"
+              }
+            ],
+            "date": "2025-04-02T20:00:00-0800",
+            "ville": "Montréal",
+            "lieu": "Maison de la Culture Ahuntsic-Cartierville",
+            "prix": "30",
+            "participants": [
+            {  
+                "idInvite": 52459,
+                "interessé":"2",
+              },
+               {  
+                "idInvite": 51843,
+                "interessé":"3",
+              },
+               {  
+                "idInvite": 51455,
+                "interessé":"2",
+              },
+               {  
+                "idInvite": 51387,
+                "interessé":"3",
+              }
+            ]
+            },
+          {
+          "id": "11129130",
+          "type": "Concert",
+          "titre": "Les Hay Babies au Club Square Dix30",
+          "genre": "folk",
+          "poster": "poster-les-hay-babies.jpg",
+          "uri": "11129130-les-hay-babies",
+          "artiste": [
+            {  
+                "artiste_id": 29837,
+                "artiste_uri":"29837-les-hey-babies",
+                "artiste_nom":"Les Hay Babies"
+              }
+            ],
+            "date": "2025-04-03T20:00:00-0800",
+            "ville": "Brossard",
+            "lieu": "Le Club Square Dix30",
+            "prix": "30", 
+            "participants": [
+            {  
+                "idInvite": 51589,
+                "interessé":"2",
+              },
+               {  
+                "idInvite": 51852,
+                "interessé":"3",
+              }
+            ]
+          },
+          {
+          "id": "11129131",
+          "type": "Concert",
+          "titre": "BB Jacques au Théâtre Fairmont Theatre",
+          "genre": "rap",
+          "poster": "poster-bb-jacques.jpg",
+          "uri": "11129131-bb-jacques",
+          "artiste": [
+            {  
+                "artiste_id": 29839,
+                "artiste_uri":"29839-bb-jaqcques",
+                "artiste_nom":"B.B Jacques"
+              }
+            ],
+            "date": "2025-04-11T20:00:00-0800",
+            "ville": "Montreal",
+            "lieu": "Théâtre Fairmont Theatre",
+            "prix": "25",
+             "participants": [
+            {  
+                "idInvite": 51889,
+                "interessé":"2",
+              },
+               {  
+                "idInvite": 51322,
+                "interessé":"3",
+              },
+               {  
+                "idInvite": 51766,
+                "interessé":"2",
+              },
+               {  
+                "idInvite": 51997,
+                "interessé":"2",
+              }
+            ]
+          },
+                  {
+          "id": "11129132",
+          "type": "Concert",
+          "titre": "Roch Voisine à la Salle Wilfrid-Pelletier, Place des Arts",
+          "genre": "rock",
+          "poster": "poster-roch-voisine.jpg",
+          "uri": "11129132-roch-voisine",
+          "artiste": [
+            {  
+                "artiste_id": 29838,
+                "artiste_uri":"29838-roch-voisine",
+                "artiste_nom":"Roch Voisine"
+              }
+            ],
             "date": "2025-04-18T20:00:00-0800",
-            "ville":"Longueuil",
-            "lieu": "Théâtre de la Ville"
-          }
+            "ville":"Montreal",
+            "lieu": "Salle Wilfrid-Pelletier, Place des Arts",
+            "prix": "80",
+            "participants": [
+            {  
+                "idInvite": 51399,
+                "interessé":"2",
+              },
+               {  
+                "idInvite": 51242,
+                "interessé":"3",
+              },
+               {  
+                "idInvite": 51756,
+                "interessé":"3",
+              },
+               {  
+                "idInvite": 51467,
+                "interessé":"2",
+              }
+            ]
+          },
+                  {
+          "id": "11129133",
+          "type": "Concert",
+          "titre": "Mario Pelchat au Théâtre Manuvie",
+          "genre": "Rock",
+          "poster": "poster-mario-pelchat.jpg",
+          "uri": "11129133-mario-pelchat",
+          "artiste": [
+            {  
+                "artiste_id": 29840,
+                "artiste_uri":"29840-mario-pelchat",
+                "artiste_nom":"Mario Pelchat"
+              }
+            ],
+            "date": "2025-04-23T20:00:00-0800",
+            "ville": "Brossard",
+            "lieu": "Théâtre Manuvie",
+            "prix": "65",
+            "participants": [
+            {  
+                "idInvite": 51609,
+                "interessé":"2",
+              },
+               {  
+                "idInvite": 51502,
+                "interessé":"3",
+              },
+               {  
+                "idInvite": 51506,
+                "interessé":"3",
+              },
+               {  
+                "idInvite": 51507,
+                "interessé":"2",
+              }
+            ]
+          },
         ];
         
 module.exports = evenementsTest;
--- index.js
+++ index.js
@@ -9,23 +9,65 @@ dotenv.config();
 
 const port = process.env.PORT;
 
+
+serveur.use(express.json()); //Permet de passer la donnée json dans le body
+serveur.use(express.urlencoded({extended:true})); //Permet de passer la donnee de formulaire
+
+
 // pour pouvoir avoir accès aux dossiers public comme assets/CSS/style.css 
 const dossierPublic = path.join(__dirname, "public"); 
 serveur.use(express.static(dossierPublic));
 
 //Middleware
-// function authentifier(req, res, next){
-//     console.log("authentification en cours");
-//     next();
-// }
+function authentifier(req, res, next){
+    console.log("authentification en cours");
+    next();
+}
 
+
+
+serveur.get("/concerts/:id", async (req, res) => {
+    //Destructuration
+    const { id } = req.params;
+
+    const docRef = await db.collection("concerts").doc(id).get();
+    const concert = { id: docRef.id, ...docRef.data() };
+
+    return res.json(concert);
+});
+
+
+serveur.get("/concerts/:date", async function (req, res){
+
+       const {date} = req.params;
+
+       const objDb = await db.collection("concerts").doc(id).get();
+
+       const concert =  { id:objDb.id, ...objDb.data() };
+    
+        return res.status(200).json(concert);
+
+});
 
 serveur.get("/concerts", async function (req, res){
-    const docs = await db.collection("concerts").get();
     
+    try {
+    const docs = await db.collection("concerts").get(); // la variable doc contient le contenu de la collection recherchée ( dans ce cas-ci collection concerts )
+    
+     const { ordre = "date", direction = "asc" } = req.query;
+
+
+        //dans le cas ou on cherche dans une collection qui n'a pas de films
+        if(docs.size == 0){
+            const reponse = {
+                msg: "Aucun concert trouvé",
+            };
+            return res.status(400).json(reponse);
+        }
+
     const concerts = [];
 
-    docs.forEach((doc)=>{
+    docs.forEach((doc) => {
         const concert = doc.data();
         concerts.push(concert);
     });
@@ -34,7 +76,59 @@ serveur.get("/concerts", async function (req, res){
     // res.setHeader("content-type", "text/plain");
     // res.send("Allo");
     // res.end("<h1>Contact</h1>");
+
+    } catch (erreur) {
+        const reponse = {
+            msg : erreur,
+        };
+        return res.status(400).json(reponse);
+    }
+        
 });
+
+
+
+serveur.post("/concerts/initialiser", async (req, res)=>{
+    
+    const concertDepart = require("./data/dataTest");
+
+    concertDepart.forEach(async (concert) => {
+        const refs = await db.collection("concerts").where("titre", "==", concert.titre).get();
+
+        if(refs.size == 0) {
+            await db.collection("concerts").add(concert);
+            console.log("ajouté");
+        } else {
+            console.log("déjà présent");
+        }
+        
+    });
+
+    const reponse = {
+        msg: "La base de donnée est initialisée",
+    };
+    return res.status(200).json(reponse);
+});
+
+
+
+serveur.post("/concerts", async (req, res)=>{
+    
+    const { body } = req;
+
+
+
+    const dateModif = dayjs().utc().toIsoString();
+    console.log(dateModif);
+    body.dateModif=dateModif;
+
+    await db.collection("concerts").add(body);
+
+    return res.status(201).json({ msg: "Le film a été ajouté" });
+});
+
+
+
 
 
 // serveur.get("/concerts/artiste", async function (req, res){
@@ -51,18 +145,88 @@ serveur.get("/concerts", async function (req, res){
 // });
 
 
-serveur.get("/users", async function (req, res){
-    const docs = await db.collection("users").get();
+
+// serveur.post("users/inscription", 
+//     [
+//     check("courriel").escape().trim().notEmpty.isEmail().normalizeEmail(),
+//     check("mdp").escape().trim().notEmpty.isStrongPassword({ minLength: 8, minNumber:1, minLowercase: 1, minUppercase: 1, minSymbols: 1 }),
+//     ],
+//      async (req,res) => {
+//     // valider les infos (express validator)
+//     // récuperer les infos du body avec identifiant unique, mot de passe /on l'envoie dans le body
+//     // vérifier si user existe
+//     // Encrypter le mot de passe
+//     // Ajouter l'user à la db
+
+
+//     // valider les infos ( est-ce que courriel et mdp existe )
+//      // À faire
+//     const erreursValidation = validationResult(req);
+
+//     if(!erreursValidation.isEmpty()){
+//         return res.status(400).json({msg: "Données invalides"});
+//     }
+       
+
+//     // récuperer les infos du body avec identifiant unique, mot de passe
+//     const { courriel, mdp, tel }  = req.body;
+
+//     // vérifier si user existe
+//     const userRefs = await db.collection("users").where("courriel", "==", courriel).get();
+//         //.docs est un tableau avec toutes les infos
+//     if(userRefs.docs.length>0){
+//         return res.status(400).json({msg: "user existant"});
+//     }    
+//     // Encrypter le mot de passe
+//     const hash = await bcrypt.hash(mdp, 10); //Le nombre de tours qu'on fait pour encrypter
+//     const user = {...req.body, 
+//         "courriel":courriel, //si ma clé est le même nom que le nom de la variable on n' pas besoin de l'ajouter
+//         mdp: hash, 
+//         tel,
+//     }
+
+//     // Ajouter l'user à la db
+//     // await db.collection("users").add(req.body);
+//     await db.collection("users").add(user);
+
+//     return res.status(201).json({msg: "L'user a été créé"});
+// });
+
+
+
+// serveur.post("/users/connexion", async () => {});
+//     //Valider les donneés
+
+//     //Récupérer les infos du body avec identifiant unique 
+//     const { courriel, mdp, tel }  = req.body;
+
+//     // récuperer les infos du body avec identifiant unique, mot de passe /on l'envoie dans le body
+//     // vérifier si user existe
+//     const userRefs = await db.collection("users").where("courriel", "==", courriel).get();
+//     if(userRefs.docs.length>0){
+//         return res.status(400).json({msg: "user existant"});
+//     }    
     
-    const users = [];
+//     // Vérifie le mot de passe
+//     const utilistateur = userRefs.docs[0].data();
 
-    docs.forEach((doc)=>{
-        const user = doc.data();
-        users.push(user);
-    });
+//     const motsPassesPareils = await bcrypt.compare(mdp, user.mdp);
 
-    return res.status(200).json(users);
-});
+//     if(motsPassesPareils){
+//         //On retourne une authentification
+//         delete utilistateur.mdp;
+//         const option = {
+//             "expiresIn":"1d", //pour desjardins par exemple on mettrait 15m
+//         };
+//         const jeton = jwt.sign(user, process.env.JWT_SECRET, options);
+
+//         return res.status(200).json({msg: "user connecté", user});
+//     }else{
+//         return res.status(400).json({msg: "Mot de passe invalide"});
+//     }
+//     // On retourne une authentification
+
+
 
 
 
